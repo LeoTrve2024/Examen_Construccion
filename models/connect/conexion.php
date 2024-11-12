5f33a51bee6e483c1ce50806c439da97242615b9
LeoTrve2024 <leonardo.travezano@unas.edu.pe>
2024-11-12 08:49:33 -0500

    Add files via upload

---
+++ models/connect/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+class conexion{
+    //public
+
+    //protegida
+
+    //privada
+    private $host;
+    private $namedb;
+    private $userdb;
+    private $passwordb;
+    private $charset;
+}
+
+?>
