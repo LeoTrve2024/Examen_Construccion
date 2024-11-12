083cd4b20187ffcf95054dbd97ce332ea90de717
LeoTrve2024 <leonardo.travezano@unas.edu.pe>
2024-11-12 08:36:15 -0500

    Add files via upload

---
+++ claveequivocada.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clave Equivocada</title>
+    <link rel="stylesheet" href="estiloclave.css">
+</head>
+<body>
+    <div class="container">
+        <h1>CLAVE EQUIVOCADA</h1>
+        <p>La contraseña que ingresaste es incorrecta.</p>
+        <a href="http://127.0.0.1/sistema" class="back-link">Volver al Login</a>
+    </div>
+</body>
+</html>
