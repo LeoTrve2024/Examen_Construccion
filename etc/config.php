caa6761958b6f4237540b04ea20c43e761e5b0c4
Leonardo Benjamin Travezanio Rodriguez <https://github.com/LeoTrve2024/Examen_Construccion.git>
2024-11-26 10:01:24 -0500

    Cambios realizados

--- etc/config.php
+++ etc/config.php
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-$_urlBase = "http://sistema.test/";
-$host = 'localhost'; //127.0.0.1
-$namedb = 'dbsistema';
-$userdb = 'root';
-$paswordb = ''; 
-
-
-function get_UrlBase($arg1){
-    return $GLOBALS['_urlBase'].$arg1;
-}
-
-function get_views($arg1){
-    return $GLOBALS['_urlBase'].'views/'.$arg1;
-}
-
-function get_models($arg1){
-    return $GLOBALS['_urlBase'].'models/'.$arg1;
-}
-
-function get_controllers($arg1){
-    return $GLOBALS['_urlBase'].'controllers/'.$arg1;
-}
-
-//echo $_urlBase;
-//echo "<br>";
-//echo get_UrlBase('pagina.html');
-
-<?php
-
-$_urlBase = "http://sistema.test/";
-$host = 'localhost'; //127.0.0.1
-$namedb = 'dbsistema';
-$userdb = 'root';
-$paswordb = ''; 
-
-
-function get_UrlBase($arg1){
-    return $GLOBALS['_urlBase'].$arg1;
-}
-
-function get_views($arg1){
-    return $GLOBALS['_urlBase'].'views/'.$arg1;
-}
-
-function get_models($arg1){
-    return $GLOBALS['_urlBase'].'models/'.$arg1;
-}
-
-function get_controllers($arg1){
-    return $GLOBALS['_urlBase'].'controllers/'.$arg1;
-}
-
-//echo $_urlBase;
-//echo "<br>";
-//echo get_UrlBase('pagina.html');
-=======
 <?php
 
 define('URL_BASE', "http://sistema.test/");
@@ -94,6 +35,3 @@ function get_controllers($arg1){
 //echo URL_BASE;
 //echo "<br>";
 //echo get_UrlBase('pagina.html');
->>>>>>> 7be4842 (Tercer hito del proyecto)
->>>>>>> 0a0cb5f (Hito 3 de proyecto)
-?>
